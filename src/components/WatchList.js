b1727aceaf48c2922e48718d1d5842e3b697255d
mkirsh123 <142982085+mkirsh123@users.noreply.github.com>
2025-07-10 13:47:40 +0530

    Add files via upload

---
+++ src/components/WatchList.js
@@ -0,0 +1,112 @@
+import React, { useState, useContext } from "react";
+
+import { Tooltip, Grow } from "@mui/material";
+import { watchlist } from "../data/data";
+
+import {
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  BarChartOutlined,
+  MoreHoriz,
+} from "@mui/icons-material";
+import GeneralContext from "./GeneralContext";
+
+const WatchList = () => {
+  return (
+    <div className="watchlist-container">
+      <div className="search-container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
+          className="search"
+        />
+        <span className="counts"> {watchlist.length} / 50</span>
+      </div>
+
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showWatch, setShowWatch] = useState(false);
+  const handleMouseEnter = (e) => {
+    setShowWatch(true);
+  };
+  const handleMouseExit = (e) => {
+    setShowWatch(false);
+  };
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className="down" />
+          ) : (
+            <KeyboardArrowUp className="down" />
+          )}
+          <span className="price">{stock.price}</span>
+        </div>
+      </div>
+      {showWatch && <WatchListActions uid={stock.name} />}
+    </li>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  const generalContext = useContext(GeneralContext);
+
+  const handleBuyClick = () => {
+    generalContext.openBuyWindow(uid);
+  };
+
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+          onClick={handleBuyClick}
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+        <Tooltip
+          title="Sell (S)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="sell">Sell</button>
+        </Tooltip>
+        <Tooltip
+          title="Analytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+        <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
