94a2dc43db8c2a39443efdafb0c16e20e33cb670
mkirsh123 <142982085+mkirsh123@users.noreply.github.com>
2026-03-03 00:51:36 +0530

    Integrate Express server with API endpoints
    Added Express server setup and API endpoints for orders, holdings, and positions.

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,18 +1,63 @@
-import React from "react";
-import { Link } from "react-router-dom";
+require("dotenv").config();
 
-const Orders = () => {
-  return (
-    <div className="orders">
-      <div className="no-orders">
-        <p>You haven't placed any orders today</p>
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-        <Link to={"/"} className="btn">
-          Get started
-        </Link>
-      </div>
-    </div>
-  );
-};
+const { HoldingsModel } = require("./models/HoldingsModel");
+const { PositionsModel } = require("./models/PositionsModel");
+const { OrdersModel } = require("./models/OrdersModel");
 
-export default Orders;
+const app = express();
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+  let allPositions = await PositionsModel.find({});
+  res.json(allPositions);
+});
+
+app.get("/allOrders", async (req, res) => {
+  let allOrders = await OrdersModel.find({});
+  res.json(allOrders);
+});
+
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel([{
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  }]);
+
+  newOrder.save();
+  temPositions.forEach((item) => {
+    let newPositions = new PositionsModel({
+      product: item.product,
+      name: item.name,
+      qty: item.qty,
+      avg: item.avg,
+      price: item.price,
+      net: item.net,
+      day: item.day,
+      isLoss: item.isLoss,
+    });
+    newPositions.save();
+  });
+  res.send("done");
+});
+
+app.listen(PORT, () => {
+  console.log(`app started on the port ${PORT}`);
+  mongoose.connect(uri);
+  console.log("DB Started!");
+});
